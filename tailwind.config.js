519ce2ae0a860d4606a4cc56bb023316e7893f8b
shodiq arifin <shodiq.arifin03@gmail.com>
2023-07-09 21:36:28 +0700

    testing

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
